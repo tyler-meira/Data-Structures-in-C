0e52d65a65119fe7b1fbc8a163340b1bba470f6a
Tyler <tyler.meira@hotmail.ca>
2024-09-19 20:24:36 -0400

    Testing Pointers

--- SingleLinked.c
+++ SingleLinked.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+To access members of a struct use the dot operator,
+to access members of a strcut through a pointer use the arrow operator.
+
+*/
+
+//Linked List Structure
+    typedef struct LinkedList{
+        int value;
+        int *next;
+    }linkedlist;
+
+    //Creates the structure object 
+    linkedlist one;
+    linkedlist two;
 
 int main(void){
-    printf("Hello World");
-    return 0;
+
+    //Creates a ptr that points the structure object
+    linkedlist *ptr = &one;
+    linkedlist *ptr2 = &two;
+    
+    ptr->value = 5;
+
+    printf("%d",ptr->value);
+
+
+
+
+
+
 }
